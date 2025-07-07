8898da3e55d6b793e18853f6b42c64cafdfb4e6a
ddanieldma <danielalmemiranda@gmail.com>
2025-07-07 05:35:48 +0000

    celery imports more robust

--- celery_app/__init__.py
+++ celery_app/__init__.py
@@ -1,9 +1,10 @@
 from celery import Celery
 
 # Creating main celery instance.
-app = Celery('scalable_computing_project')
+app = Celery(
+    'scalable_computing_project',
+    include=['celery_app.tasks']
+)
 
 # Loading settings
 app.config_from_object('celery_app.celery_config')
-
-app.autodiscover_tasks()
